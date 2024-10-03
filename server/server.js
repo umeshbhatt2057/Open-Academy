9f12133516131c7623e9178f48aea90b02b00f15
Umesh Bhatt <100942158+umcBhatt@users.noreply.github.com>
2024-10-03 22:12:16 +0545

    server.js file made

---
+++ server/server.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const express = require ("express");
+const cors = require("cors");
+const mongoose = require('mongoose');
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+cors({
+    origin : process.env.CLIENT_URL,
+    method : ["GET","POST","DELETE","PUT"],
+    allowedHeaders: ["Content-Type","Authorization"], 
+});
+
+app.use(express.json());
+
+//database connection
+mongoose
+.connect(MONGO_URI)
+.then(()=> console.log("MongodB is connected"))
+.catch((e)=> console.log(e));
+
+
+//routes configuration
+
+
+
+app.use((err,req,res,next)=>{
+    console.log(err.stack);
+    res.status(500).json({
+        success : false,
+        message : "Something went wrong",
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is now running on port ${PORT}`);
+})
+
