65e8c208ed3e699f5a9d57b0dbfb59bf24a98dbb
Umesh Bhatt <100942158+umcBhatt@users.noreply.github.com>
2024-10-03 21:14:12 +0545

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+import { Button } from './components/ui/button'
+
+function App() {
+  
+
+  return (
+    <>
+     <Button className=" bg-red-500" >Umesh</Button>
+    </>
+  )
+}
+
+export default App
