b6056fef2cb38daba20132d592e5e8a141671ef4
Umesh Bhatt <100942158+umcBhatt@users.noreply.github.com>
2024-10-06 21:26:33 +0545

    improved the formatting

--- client/src/context/auth-context/index.jsx
+++ client/src/context/auth-context/index.jsx
@@ -1,26 +1,22 @@
-import { initialSignInFormData, initialSignUpFormData } from "@/config";
 import { createContext, useState } from "react";
-
+import { initialSignInFormData, initialSignUpFormData } from "@/config";
 
 export const AuthContext = createContext(null);
 
-
 export default function AuthProvider({ children }) {
-
-    const[signInFormData,setSignInFormData] = useState(initialSignInFormData) 
-    const[signUpFormData,setSignUpFormData] = useState(initialSignUpFormData)
-
+    const [signInFormData, setSignInFormData] = useState(initialSignInFormData);
+    const [signUpFormData, setSignUpFormData] = useState(initialSignUpFormData);
 
     return (
-    <AuthContext.Provider
-     value={{
-
-        signInFormData,
-        setSignInFormData,
-        signUpFormData,
-        setSignUpFormData
-    }}
-    >{children}
-    </AuthContext.Provider>
+        <AuthContext.Provider
+            value={{
+                signInFormData,
+                setSignInFormData,
+                signUpFormData,
+                setSignUpFormData,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
     );
 }
